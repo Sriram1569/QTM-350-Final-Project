f0028f6ad63c6212b508f96e020ad920dfe9c351
JusHe1 <157382189+JusHe1@users.noreply.github.com>
2025-12-07 16:47:16 -0500

    Update clean.sql

--- scripts/clean.sql
+++ scripts/clean.sql
@@ -1,17 +1,14 @@
 
--- Step 1: 创建 GDP per capita 表
 DROP TABLE IF EXISTS gdp_per_capita;
 CREATE TABLE gdp_per_capita AS
 SELECT *
 FROM read_csv_auto('data/raw/API_NY.GDP.PCAP.KD_DS2_en_csv_v2_2624.csv');
 
--- Step 2: 创建 GDP growth 表
 DROP TABLE IF EXISTS gdp_growth;
 CREATE TABLE gdp_growth AS
 SELECT *
 FROM read_csv_auto('data/raw/API_NY.GDP.MKTP.KD.ZG_DS2_en_csv_v2_2509.csv');
 
--- Step 3: 创建 employment rate 表
 DROP TABLE IF EXISTS employment_rate;
 CREATE TABLE employment_rate AS
 SELECT *
